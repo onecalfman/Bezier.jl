c4bef7c615b609810b6aa476f0a581c4a94d993b
Jonas Walkling <jonas@walkling.email>
2022-05-15 14:53:14 +0200

    more consistent code formatting

--- src/Bezier.jl
+++ src/Bezier.jl
@@ -14,30 +14,19 @@ function rowtocolumn(vec::AbstractVector{T}) where T <: AbstractVector
     return vects
 end
 
-# actual calculation for quadratic
-bezier(p0 :: Vector, p1 :: Vector, p2 :: Vector, t :: Real) =
-  (1-t)^2 * ((1-t) * p0 + t * p1) + t * ((1-t) * p1 + t * p2)
-
-# for cubic
-bezier(p0 :: Vector, p1 :: Vector, p2 :: Vector, p3 :: Vector, t :: Real) =
-  (1-t)^3 * p0 + 3 * (1-t)^2 * t * p1 + 3 * (1-t)*t^2 * p2 + t^3 * p3
+# actual calculation for quadratic and cubic curves
+bezier(p0 :: Vector, p1 :: Vector, p2 :: Vector,               t :: Real) = (1-t)^2 * ((1-t) * p0 + t * p1) + t * ((1-t) * p1 + t * p2)
+bezier(p0 :: Vector, p1 :: Vector, p2 :: Vector, p3 :: Vector, t :: Real) = (1-t)^3 * p0 + 3 * (1-t)^2 * t * p1 + 3 * (1-t)*t^2 * p2 + t^3 * p3
 
 # returns two lists with the x and y values for the given points
 # it can be used for plotting like this:
 #   plot(bezier([0,0],[1,0],[1,1])...)
-function bezier(p0 :: Vector, p1 :: Vector, p2 :: Vector; range = 0:0.01:1)
-	map(t -> bezier(p0,p1,p2,t), range) |> rowtocolumn
-end
-
-function bezier(p0 :: Vector, p1 :: Vector, p2 :: Vector, p3 :: Vector; range = 0:0.01:1)
-	map(t -> bezier(p0,p1,p2,p3,t), range) |> rowtocolumn
-end
+bezier(p0 :: Vector, p1 :: Vector, p2 :: Vector;               range = 0:0.01:1) = map(t -> bezier(p0,p1,p2,t), range) |> rowtocolumn
+bezier(p0 :: Vector, p1 :: Vector, p2 :: Vector, p3 :: Vector; range = 0:0.01:1) = map(t -> bezier(p0,p1,p2,p3,t), range) |> rowtocolumn
 
 # accepts two lists of x and y values.
 # The following produces the same result as the example above:
 #   plot(bezier([0,1,1],[0,0,1])...)
-function bezier(r1 :: Vector, r2 :: Vector; range = 0:0.01:1)
-	bezier(([r1, r2] |> rowtocolumn)..., range = range)
-end
+function bezier(r1 :: Vector, r2 :: Vector; range = 0:0.01:1) = bezier(([r1, r2] |> rowtocolumn)..., range = range)
 
 end # module
