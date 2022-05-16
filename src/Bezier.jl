e1e60b9985853f3452ac3199857bbd394b40e5a1
Jonas Walkling <jonas@walkling.email>
2022-05-16 13:20:34 +0200

    rename function

--- src/Bezier.jl
+++ src/Bezier.jl
@@ -3,7 +3,7 @@ module Bezier
 export bezier
 
 # https://stackoverflow.com/questions/63892334/using-broadcasting-julia-for-converting-vector-of-vectors-to-matrices
-function rowtocolumn(vec::AbstractVector{T}) where T <: AbstractVector
+function switch_dimensions(vec::AbstractVector{T}) where T <: AbstractVector
     dim1 = length(vec)
     dim2 = length(vec[1])
     vects = Vector{Vector}(undef, dim2)
@@ -21,12 +21,12 @@ bezier(p0 :: Vector, p1 :: Vector, p2 :: Vector, p3 :: Vector, t :: Real) = (1-t
 # returns two lists with the x and y values for the given points
 # it can be used for plotting like this:
 #   plot(bezier([0,0],[1,0],[1,1])...)
-bezier(p0 :: Vector, p1 :: Vector, p2 :: Vector;               range = 0:0.01:1) = map(t -> bezier(p0,p1,p2,t), range) |> rowtocolumn
-bezier(p0 :: Vector, p1 :: Vector, p2 :: Vector, p3 :: Vector; range = 0:0.01:1) = map(t -> bezier(p0,p1,p2,p3,t), range) |> rowtocolumn
+bezier(p0 :: Vector, p1 :: Vector, p2 :: Vector;               range = 0:0.01:1) = map(t -> bezier(p0,p1,p2,t), range) |> switch_dimensions
+bezier(p0 :: Vector, p1 :: Vector, p2 :: Vector, p3 :: Vector; range = 0:0.01:1) = map(t -> bezier(p0,p1,p2,p3,t), range) |> switch_dimensions
 
 # accepts two lists of x and y values.
 # The following produces the same result as the example above:
 #   plot(bezier([0,1,1],[0,0,1])...)
-bezier(r1 :: Vector, r2 :: Vector; range = 0:0.01:1) = bezier(([r1, r2] |> rowtocolumn)..., range = range)
+bezier(r1 :: Vector, r2 :: Vector; range = 0:0.01:1) = bezier(([r1, r2] |> switch_dimensions)..., range = range)
 
 end # module
