97f8efc23465350929ed27ac303661647bd7e5c2
Jonas Walkling <jonas@walkling.email>
2022-05-23 22:33:15 +0200

    convert named range args to positional

--- src/Bezier.jl
+++ src/Bezier.jl
@@ -9,11 +9,11 @@ bernstein(n,t,w) = mapreduce(k -> bernstein(n,t,k,w), (a,b) -> a + b, 0:n)
 
 bezier(n :: Integer, t :: Float64, w :: AbstractVector{T}) where T <: Real = reduce(t -> bernstein(n,t,k,w), w)
 
-bezier(x :: Vector{T}; range = 0:0.01:1) where T <: Real = map(t -> bernstein((x |> length) - 1, t, x), range)
+bezier(x :: Vector{T}, range = 0:0.01:1) where T <: Real = map(t -> bernstein((x |> length) - 1, t, x), range)
 
-bezier(x :: Vector{T}, y :: Vector{S}; range = 0:0.01:1) where {T,S <: Number} = (bezier(x, range = range), bezier(y, range = range))
+bezier(x :: Vector{T}, y :: Vector{S}, range = 0:0.01:1) where {T,S <: Number} = (bezier(x, range = range), bezier(y, range = range))
 
-bezier(x :: AbstractMatrix{T}, y :: AbstractMatrix{S}; range = 0:0.01:1) where {T,S <: Number} = bezier([x y], range = range)
+bezier(x :: AbstractMatrix{T}, y :: AbstractMatrix{S}, range = 0:0.01:1) where {T,S <: Number} = bezier([x y], range = range)
 
 function bezier(x :: AbstractMatrix{T}, range = 0:0.01:1) where T <: Number
 	if size(x)[2] == 2
