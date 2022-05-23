468d05fd38bd8d69273a404e2e9089ccaf094e6c
Jonas Walkling <jonas@walkling.email>
2022-05-23 22:20:11 +0200

    redone

--- src/Bezier.jl
+++ src/Bezier.jl
@@ -2,31 +2,27 @@ module Bezier
 
 export bezier
 
-# https://stackoverflow.com/questions/63892334/using-broadcasting-julia-for-converting-vector-of-vectors-to-matrices
-function switch_dimensions(vec::AbstractVector{T}) where T <: AbstractVector
-    dim1 = length(vec)
-    dim2 = length(vec[1])
-    vects = Vector{Vector}(undef, dim2)
-    map(x -> vects[x] = Array{eltype(vec[1])}(undef, dim1), 1:dim2)
-    @inbounds @fastmath for i in 1:dim1, j in 1:dim2
-        vects[j][i] = vec[i][j]
-    end
-    return vects
+
+bernstein(n :: Integer, t :: T,k :: Integer, w :: AbstractVector{S}) where {T,S <: Number} = binomial(n,k) * (1-t)^(n-k) * t^k * w[k+1]
+
+bernstein(n,t,w) = mapreduce(k -> bernstein(n,t,k,w), (a,b) -> a + b, 0:n)
+
+bezier(n :: Integer, t :: Float64, w :: AbstractVector{T}) where T <: Real = reduce(t -> bernstein(n,t,k,w), w)
+
+bezier(x :: Vector{T}; range = 0:0.01:1) where T <: Real = map(t -> bernstein((x |> length) - 1, t, x), 0:0.01:1)
+
+bezier(x :: Vector{T}, y :: Vector{S}; range = 0:0.01:1) where {T,S <: Number} = (bezier(x, range = range), bezier(y, range = range))
+
+bezier(x :: AbstractMatrix{T}, y :: AbstractMatrix{S}; range = 0:0.01:1) where {T,S <: Number} = bezier([x y], range = range)
+
+function bezier(x :: AbstractMatrix{T}) where T <: Number
+	if size(x)[2] == 2
+		bezier(vec(x[1,:]),vec(y[2,:]))
+	elseif size(x)[1] == 2
+		bezier(vec(x[:,]),vec(y[:,2]))
+	else
+		throw(ArgumentError("unsupported matrix dimensions"))
+	end
 end
-
-# actual calculation for quadratic and cubic curves
-bezier(p0 :: Vector, p1 :: Vector, p2 :: Vector,               t :: Real) = (1-t)^2 * ((1-t) * p0 + t * p1) + t * ((1-t) * p1 + t * p2)
-bezier(p0 :: Vector, p1 :: Vector, p2 :: Vector, p3 :: Vector, t :: Real) = (1-t)^3 * p0 + 3 * (1-t)^2 * t * p1 + 3 * (1-t)*t^2 * p2 + t^3 * p3
-
-# returns two lists with the x and y values for the given points
-# it can be used for plotting like this:
-#   plot(bezier([0,0],[1,0],[1,1])...)
-bezier(p0 :: Vector, p1 :: Vector, p2 :: Vector;               range = 0:0.01:1) = map(t -> bezier(p0,p1,p2,t), range) |> switch_dimensions
-bezier(p0 :: Vector, p1 :: Vector, p2 :: Vector, p3 :: Vector; range = 0:0.01:1) = map(t -> bezier(p0,p1,p2,p3,t), range) |> switch_dimensions
-
-# accepts two lists of x and y values.
-# The following produces the same result as the example above:
-#   plot(bezier([0,1,1],[0,0,1])...)
-bezier(r1 :: Vector, r2 :: Vector; range = 0:0.01:1) = bezier(([r1, r2] |> switch_dimensions)..., range = range)
 
 end # module
