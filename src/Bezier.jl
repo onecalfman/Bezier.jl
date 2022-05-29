509bd8e0a61054054e117b0c399d6e17a6037ce8
Jonas Walkling <jonas@walkling.email>
2022-05-29 22:46:34 +0200

    tidying up

--- src/Bezier.jl
+++ src/Bezier.jl
@@ -2,26 +2,26 @@ module Bezier
 
 export bezier
 
+bernstein(n :: Integer, t :: T, k :: Integer, w :: AbstractVector{S}) where {T,S <: Real} = binomial(n,k) * (1-t)^(n-k) * t^k * w[k+1]
 
-bernstein(n :: Integer, t :: T,k :: Integer, w :: AbstractVector{S}) where {T,S <: Number} = binomial(n,k) * (1-t)^(n-k) * t^k * w[k+1]
+function bernstein(t :: T, w :: AbstractVector{S}) where {T,S <: Real}
+    n :: Integer = length(w) - 1
+    mapreduce(k -> bernstein(n,t,k,w), (a,b) -> a + b, 0:n)
+end
 
-bernstein(n,t,w) = mapreduce(k -> bernstein(n,t,k,w), (a,b) -> a + b, 0:n)
+bezier(x :: AbstractVector{T}, range :: StepRangeLen = 0:0.01:1) where T <: Real = map(t -> bernstein(t, x), range)
 
-bezier(n :: Integer, t :: Float64, w :: AbstractVector{T}) where T <: Real = reduce(t -> bernstein(n,t,k,w), w)
+bezier(x :: AbstractVecOrMat{T}, y :: AbstractVecOrMat{S}, range :: StepRangeLen = 0:0.01:1) where {T,S <: Real} = (bezier(x, range), bezier(y, range))
 
-bezier(x :: Vector{T}, range :: StepRangeLen = 0:0.01:1) where T <: Real = map(t -> bernstein((x |> length) - 1, t, x), range)
+bezier(x :: AbstractMatrix{T}, y :: AbstractMatrix{S}, range :: StepRangeLen = 0:0.01:1) where {T,S <: Real} = bezier(vec(x), vec(y), range)
 
-bezier(x :: Vector{T}, y :: Vector{S}, range :: StepRangeLen = 0:0.01:1) where {T,S <: Number} = (bezier(x, range), bezier(y, range))
-
-bezier(x :: AbstractMatrix{T}, y :: AbstractMatrix{S}, range :: StepRangeLen = 0:0.01:1) where {T,S <: Number} = bezier([x y], range)
-
-function bezier(x :: AbstractMatrix{T}, range = 0:0.01:1) where T <: Number
+function bezier(x :: AbstractMatrix{T}, range :: StepRangeLen = 0:0.01:1) where T <: Real
 	if size(x)[1] == 2
 		bezier(vec(x[1,:]),vec(x[2,:]), range)
 	elseif size(x)[2] == 2
 		bezier(vec(x[:,1]),vec(x[:,2]), range)
 	else
-		throw(ArgumentError("unsupported matrix dimensions"))
+		throw(ArgumentError("unsupported matrix dimensions, needs to be 2xm or nx2"))
 	end
 end
 
